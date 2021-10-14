0c65bf8674c86566e3fca933a11c3a9e8a3ab6ae
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-10-14 18:05:01 +0300

    на промиссах

--- src/apiService.js
+++ src/apiService.js
@@ -1,9 +1,32 @@
-export default function fetchPictures(e) {
-  const searchQuery = e.target.value;
-  const KEY = "23837167-bf1b53cb947cc958b90463dad";
+const KEY = "23837167-bf1b53cb947cc958b90463dad";
 
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${KEY}`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => data);
+export default class ApiService {
+  constructor() {
+    this.searchQuery = "";
+    this.page = 1;
+    this.perPage = 12;
+  }
+
+  fetchPictures(e) {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${KEY}`;
+
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.page += 1;
+        return data.hits;
+      });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import fetchPictures from "./apiService.js";
+import ApiService from "./apiService.js";
 import cardTmpl from "./template.hbs";
+import * as _ from "lodash";
+import "./styles.css";
 
 const inputEl = document.querySelector(".search-form");
 const container = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 
-inputEl.addEventListener("input", onInput);
+const apiService = new ApiService();
+
+inputEl.addEventListener("input", _.debounce(onInput, 600));
+loadMoreBtn.addEventListener("click", onLoadMore);
 
 function onInput(e) {
   e.preventDefault();
-  fetchPictures(e).then((data) => {
+  apiService.query = e.target.value;
+  if (apiService.query === "") {
+    return;
+  }
+  apiService.resetPage();
+  apiService.fetchPictures().then((data) => {
+    clearContainer();
     container.insertAdjacentHTML("beforeend", cardTmpl(data));
+    loadMoreBtn.disabled = false;
+  });
+}
+
+function onLoadMore() {
+  apiService.fetchPictures().then((data) => {
+    container.insertAdjacentHTML("beforeend", cardTmpl(data));
+    scroll();
+  });
+}
+
+function clearContainer() {
+  container.innerHTML = "";
+}
+
+function scroll() {
+  let lastLiItem = document.querySelectorAll(".list-item");
+
+  lastLiItem[lastLiItem.length - (apiService.perPage - 1)].scrollIntoView({
+    behavior: "smooth",
+    block: "end",
   });
 }
